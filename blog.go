b8a9876afa3b8075b46b4e8f047e71a50bff12f6
Damian Nicholson <damian.nicholson21@gmail.com>
2014-10-23 00:21:34 +0100

    File restructure
    - Reorganised code
    - Introduced concept of blog has_many pages
    - Blog now accepts a dir, which then globs through the files contained
      within it

---
+++ blog.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "io/ioutil"
+  "fmt"
+)
+
+type Blog struct {
+  pages []*Page
+}
+
+func NewPages(dirname string) (*Blog, error) {
+  blog := Blog{}
+
+  files, _ := ioutil.ReadDir(dirname)
+  for _, f := range files {
+    fullpath := fmt.Sprintf("%s%s", dirname, f.Name())
+    page, err := NewPage(fullpath)
+    if err != nil {
+      fmt.Println("File error: %v", err)
+    }
+
+    fmt.Println(page)
+    blog.pages = append(blog.pages, page)
+    page.save()
+  }
+
+  return &blog, nil
+}
---
+++ page.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"path/filepath"
+  "os"
+  "fmt"
+
+	"github.com/russross/blackfriday"
+)
+
+type Page struct {
+	Title         string
+	Body          string
+	FormattedBody template.HTML
+	Filename      string
+}
+
+func NewPage(filename string) (*Page, error) {
+	file, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	page := Page{}
+	err = json.Unmarshal(file, &page)
+
+	formattedBody := blackfriday.MarkdownCommon([]byte(page.Body))
+	page.FormattedBody = template.HTML(formattedBody)
+	page.Filename = parseFilename(filename)
+
+	return &page, nil
+}
+
+func (page *Page) save() error {
+  output_path := fmt.Sprintf("./_site/%s.html", page.Filename)
+  file, err := os.Create(output_path)
+  defer file.Close()
+
+  if err != nil {
+    panic(err)
+  }
+
+  tmpl, err := template.ParseFiles("layouts/application.html")
+  return tmpl.Execute(file, page)
+}
+
+func parseFilename(path string) string {
+	filename := filepath.Base(path)
+	extension := filepath.Ext(path)
+	basename := filename[:len(filename)-len(extension)]
+
+	return basename
+}
