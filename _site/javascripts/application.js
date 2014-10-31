9ebdd741a36fee5229aee19bfb3e7eadf4f3f08c
Damian Nicholson <damian.nicholson21@gmail.com>
2014-10-31 10:03:47 +0000

    Remove generated content

--- _site/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-var textarea = document.getElementsByTagName('textarea')[0];
-
-var myCodeMirror = CodeMirror.fromTextArea(textarea);
-// var myCodeMirror = CodeMirror(document.body, {
-//   mode: "markdown"
-// });
