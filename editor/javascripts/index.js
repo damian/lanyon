532cb8699fdee7bdd272a300a1a9e99cc374b30a
Damian Nicholson <damian.nicholson21@gmail.com>
2014-11-11 23:43:52 +0000

    Managed to get Ajax req going

--- editor/javascripts/index.js
+++ editor/javascripts/index.js
@@ -1,7 +1,6 @@
-angular.module('app',[]);
+angular.module('app',[])
   .controller('BlogIndexController', ['$scope', '$http', function($scope, $http) {
-  // $http.get('/api/v1/blogs').success(function(data) {
-  //   console.log(data);
-  // });
-  // console.log($scope);
+    $http.get('/api/v1/blogs').success(function(data) {
+      $scope.blogs = data.Pages;
+    });
   }]);
